98e3291ba24bb7925e1847a8e55ae2bb2f902972
CrossDJinn <crossdjinn@gmail.com>
2018-10-06 19:42:58 +0200

    0.0.0 test dev wtf?

--- examples/express-embed/server.js
+++ examples/express-embed/server.js
@@ -5,24 +5,65 @@ var express = require('express')
 var app = express()
 
 // https://wiki.theory.org/BitTorrentSpecification#peer_id
-var whitelist = {
-  UT: true // uTorrent
-}
 
 var server = new Server({
-  http: false, // we do our own
-  udp: false, // not interested
-  ws: false, // not interested
-  filter: function (params) {
-    // black/whitelist for disallowing/allowing specific clients [default=allow all]
-    // this example only allows the uTorrent client
-    var client = params.peer_id[1] + params.peer_id[2]
-    return whitelist[client]
+  http: true, // we do our own
+  udp: true, // not interested
+  ws: true, // not interested
+  stats: true, // enable web-based statistics? [default=true]
+  filter: function (infoHash, params, cb) {
+    // Blacklist/whitelist function for allowing/disallowing torrents. If this option is
+    // omitted, all torrents are allowed. It is possible to interface with a database or
+    // external system before deciding to allow/deny, because this function is async.
+
+    // It is possible to block by peer id (whitelisting torrent clients) or by secret
+    // key (private trackers). Full access to the original HTTP/UDP request parameters
+    // are available in `params`.
+
+    // This example only allows one torrent.
+
+    let allowed = true
+    if (allowed) {
+      // If the callback is passed `null`, the torrent will be allowed.
+      cb(null)
+    } else {
+      // If the callback is passed an `Error` object, the torrent will be disallowed
+      // and the error's `message` property will be given as the reason.
+      cb(new Error('disallowed torrent'))
+    }
   }
 })
 
 var onHttpRequest = server.onHttpRequest.bind(server)
 app.get('/announce', onHttpRequest)
 app.get('/scrape', onHttpRequest)
+
+server.on('error', function (err) {
+  // fatal server error!
+  console.log(err.message)
+})
+
+server.on('warning', function (err) {
+  // client sent bad data. probably not a problem, just a buggy client.
+  console.log(err.message)
+})
+
+server.on('listening', function () {
+  // fired when all requested servers are listening
+  console.log('listening on http port:' + server.http.address().port)
+  console.log('listening on udp port:' + server.udp.address().port)
+})
+
+// start tracker server listening! Use 0 to listen on a random free port.
+server.listen(2988, 'localhost', 'onlistening')
+
+// listen for individual tracker messages from peers:
+server.on('start', function (addr) {
+  console.log('got start message from ' + addr)
+})
+
+server.on('complete', function (addr) {})
+server.on('update', function (addr) {})
+server.on('stop', function (addr) {})
 
 app.listen(8080)
